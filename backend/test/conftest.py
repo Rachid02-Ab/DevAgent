9c989632d9e17efbc751d90de9068b8954019d72
0761824677 <rachidabounnaim3@gmail.com>
2025-05-01 19:12:11 +0200

    fixing pipeline

--- backend/test/conftest.py
+++ backend/test/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
-from main import app
+from app import main
 
 @pytest.fixture
 def client():
-    return TestClient(app)
+    return TestClient(main)
